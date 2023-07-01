aebdb45101b2ad16eb296c2c82e6ca5e5e29f544
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-06-30 23:33:47 -0300

    config: Removed unused font playfair

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,21 @@ module.exports = {
       fontFamily: {
         title: 'var(--font-roboto)',
         nameUser: 'var(--font-open)',
-        postTitle: 'var(--font-playfair)',
         content: 'var(--font-noto)',
         button: 'var(--font-nunito)',
 
-      }
+      },
+      colors: {
+        backgroundColor: '#FFFFFF',
+        socialFont: '#0047AB',
+        userFont: '#000000',
+        postContentFont: '#333333',
+        buttonColorFont: '#F5F5F5',
+        userBackground: '#F5F5F5',
+        buttonBackground: '#005CBF',
+        postBackground: '#F5F5F5',
+        commentBackground: '#F5F5F5',
+      },
     },
   },
   plugins: [],
